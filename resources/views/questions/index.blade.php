8e4224e03f7d08694607f9a577e421fbe0bfa7b5
Yeasir Arafat <yeasirarafat321@gmail.com>
2020-04-11 15:04:15 +0600

    Ninth Commit

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -30,7 +30,17 @@
                                     </div>
                                 </div>
                                 <div class="media-body">
-                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a> </h3>
+                                    <div class="d-flex align-items-center">
+                                        <h3 class="mt-0"><a href="{{ route('question.show',$question->id) }}">{{ $question->title }}</a> </h3>
+                                        <div class="ml-auto">
+                                            <a href="{{ route('question.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            <form class="form-delete" method="post" action="{{ route('question.destroy',$question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
                                     <p class="lead">
                                         Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                         <small class="text-muted">{{ $question->create_date }}</small>
